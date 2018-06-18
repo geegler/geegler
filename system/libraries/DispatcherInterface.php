f92782019d306139c3a3899c65671fedcaf760fc
Lorenzo De Leon Alipio <lyonne1114@gmail.com>
2018-06-17 22:05:18 -0700

    Delete DispatcherInterface.php

--- system/libraries/DispatcherInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace System\Libraries\Dispatch;
-
-Interface DispatcherInterface
-{
-	public function requestedController($controller);
-	public function requestedMethod($action);
-	public function requestedParams(array $params);
-	public function serveRequest();
-}
